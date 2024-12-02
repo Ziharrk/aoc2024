62770ff64ef6460cd053b4cb3e4a9e102a4524a4
Kai Prott <kai.prott@hotmail.de>
2024-12-02 06:28:56 +0100

    Add day2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,4 +1,31 @@
+{-# LANGUAGE LambdaCase #-}
 module Day2 where
 
+import Data.Maybe (isJust)
+import Control.Applicative (Alternative(..))
+
+numSafe :: [[Int]] -> Int
+numSafe = length . filter (isJust . isSafe)
+
+isSafe :: [Int] -> Maybe Ordering
+isSafe [x,y] | let d = abs (x - y), d >= 1, d <= 3 = return (compare x y)
+isSafe (x:y:xs) =
+  isSafe (y:xs) >>= \case
+    LT | x < y && y - x <= 3 -> return LT
+    GT | x > y && x - y <= 3 -> return GT
+    _                        -> Nothing
+isSafe _ = empty
+
+numSafeDampened :: [[Int]] -> Int
+numSafeDampened = length . filter (any @[] (isJust . isSafe) . dampen)
+  where
+    dampen []     = return []
+    dampen (x:xs) = (x:) <$> dampen xs <|> return xs
+
 day2 :: IO ()
-day2 = print "not implemented yet"
+day2 = do
+  input <- map (map read . words) . lines <$> readFile "input/day2"
+  putStr "Part 1: "
+  print $ numSafe input
+  putStr "Part 2: "
+  print $ numSafeDampened input
