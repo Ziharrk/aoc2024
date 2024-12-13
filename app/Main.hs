ba85ab564359dd00c0bce2695af13de976c954f7
Kai Prott <kai.prott@hotmail.de>
2024-12-13 07:53:42 +0100

    Update Main for convenience

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,29 @@
 module Main (main) where
 
 import Data.List.Extra ((!?))
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 import System.IO (hSetBuffering, stdin, stdout, BufferMode(..))
 
 import AllDays (allDays)
 
 main :: IO ()
 main = do
-    putStrLn "Which day?"
-    input <- getLine
-    hSetBuffering stdin  LineBuffering
-    hSetBuffering stdout NoBuffering
-    case reads input of
-        [(n, "")]
-            | n >= 1 && n <= 25 ->
-                case allDays !? (n - 1) of
-                    Just day -> day
-                    Nothing  -> putStrLn "Day not implemented yet"
-        _   -> putStrLn "Invalid day! It has to be a number between 1 and 25."
+    args <- getArgs
+    d <- case args of
+          [s] | [(n, "")] <- reads s,
+                n >= 1 && n <= 25
+             -> return n
+          [] -> do
+            putStrLn "Which day?"
+            input <- getLine
+            hSetBuffering stdin  LineBuffering
+            hSetBuffering stdout NoBuffering
+            case reads input of
+                [(n, "")] | n >= 1 && n <= 25 -> return n
+                _ -> putStrLn ("Invalid day! " ++ explanation) >> exitFailure
+          _ -> putStrLn ("Invalid command line Argument! " ++ explanation) >> exitFailure
+    case allDays !? (d - 1) of
+        Just day -> day
+        Nothing  -> putStrLn "Day not implemented yet"
+  where explanation = "It has to be a number between 1 and 25."
