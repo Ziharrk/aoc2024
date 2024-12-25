4bcd746966fd961bcde0abc73eb3b5811227832e
Kai Prott <kai.prott@hotmail.de>
2024-12-25 11:42:20 +0100

    Add day25

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,4 +1,25 @@
 module Day25 where
 
+import Data.List.Extra (wordsBy, partition, transpose)
+
+classify :: [[Char]] -> Bool
+classify []    = error "empty schematic"
+classify (x:_) = all (=='#') x
+
+getProfile :: [[Char]] -> [Int]
+getProfile = map (pred . length . takeWhile (=='#')) . transpose
+
+fits :: [Int] -> [Int] -> Bool
+fits xs = and . zipWith (\a -> (< 6) . (+a)) xs
+
 day25 :: IO ()
-day25 = print "not implemented yet"
+day25 = do
+  input <- lines <$> readFile "input/day25"
+  let schematics = wordsBy null input
+  let (locks, keys) = partition classify schematics
+  let (lockProfiles, keyProfiles) = (map getProfile locks, map (getProfile . reverse) keys)
+  print (lockProfiles, keyProfiles)
+  print (length $ filter (uncurry fits) $ allCombinations lockProfiles keyProfiles)
+
+allCombinations :: [a] -> [b] -> [(a, b)]
+allCombinations xs ys = [(x, y) | x <- xs, y <- ys]
